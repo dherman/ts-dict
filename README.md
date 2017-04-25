89baf9dc18024903a9548422b0eda4c1b6167c50
Dave Herman <dherman@mozilla.com>
2017-04-25 13:29:05 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# TypeScript Dict
+
+This TypeScript library provides a type `Dict<T>` for
+dynamic dictionary objects.
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/** Generic type of dynamic dictionary objects. */
+type Dict<T> = {
+    [key: string]: T
+}
+
+export default Dict;
